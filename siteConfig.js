8b8dbe7a6b0f61fddc2da01ea23ecd0a68e85ad6
yasuperu <info@jasperkamphuis.com>
2021-04-29 11:46:03 +0200

    change config

--- siteConfig.js
+++ siteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   shortName: "Coat",
   description:
     "A digital-driven, creative studio, focused on making new technology tangible and understandable.",
-  url: "https://studioco.at", // Domain of your site without prefix!
+  url: "https://studiocoat.nl", // Domain of your site without prefix!
   prefix: "/",
   author: "Jasper Kamphuis",
   twitter: "Imed_Adel",
